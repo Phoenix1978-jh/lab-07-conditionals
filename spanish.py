772c5bbc0395984e8e336945ec6b11bf3780cb3a
Jasmine Hernandez <bezierbee@gmail.com>
2023-10-17 22:26:22 -0400

    +spanish.py

---
+++ spanish.py
@@ -0,0 +1,10 @@
+wordinenglish = input("Give me a word for English: ")
+
+if(wordinenglish == "cat"):
+    print("gato")
+elif(wordinenglish == "dog"):
+    print("perro")
+elif(wordinenglish == "horse"):
+    print("caballo")
+else:
+    print("no entiendo")
